50ac0506cc438d13736ababf45c641cd0c22fcbd
Varun Mahajan <varunmahajan32@hotmail.com>
2024-09-01 17:08:40 -0400

    Cart icon and dropdown changes added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,14 +4,14 @@ import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <label className="item-count">0</label>
+      <label className="item-count">{cartCount}</label>
     </div>
   );
 };
